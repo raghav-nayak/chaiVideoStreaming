2d84dd012baa89620457bea26e17151fce227d02
Raghav Nayak <ragsnayak@gmail.com>
2024-02-01 23:14:07 +0530

    added file upload code

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,32 @@
+import {
+    v2 as cloudinary
+} from 'cloudinary';
+import fs from "fs";
+
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET
+});
+
+const uploadOnCloudinary = async (localFilePath) => {
+    try {
+        if (!localFilePath) return null;
+
+        //upload the file on cloudinary
+        const response = await cloudinary.uploader.upload(localFilePath, {
+            resource_type: "auto",
+        })
+
+        //file has been successfully uploaded
+        console.log("File is successfully uploaded to cloudinary", response.url); // cloudinary returns public url
+        return response
+    } catch (err) {
+        fs.unlinkSync(localFilePath); // remove local temporary file as the upload action failed
+        return null
+    }
+}
+
+export {
+    uploadOnCloudinary
+};
